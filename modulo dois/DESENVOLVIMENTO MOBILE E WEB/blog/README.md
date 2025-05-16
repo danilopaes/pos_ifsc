51fd04dcd72655124f5dc081c85a0676b39811ca
danilopaes <danilopaesribeiro@gmail.com>
2025-05-16 20:35:01 -0300

    feat: adiciona arquivos ao projeto do blog

---
+++ modulo dois/DESENVOLVIMENTO MOBILE E WEB/blog/README.md
@@ -0,0 +1,8 @@
+# Blog
+
+# Tecnologias do projeto
+
+A API é um conjunto simples de serviços presentes em um blog
+
+
+
--- modulo dois/DESENVOLVIMENTO MOBILE E WEB/blog/index.js
+++ modulo dois/DESENVOLVIMENTO MOBILE E WEB/blog/index.js
@@ -1,6 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const knex = require('./db');
+const app = express();
+app.use(express.json());
+knex.schema.hasTable('usuarios').then(exists => {
+  if (!exists) {
+    return knex.schema.createTable('usuarios', table => {
+      table.increments('id');
+      table.string('nome');
+      table.string('email');
+    });
+  }
+});
 
-app.get('/',  (req, res) => res.send ("Hello World"));
-
-app.listen(3000, () => console.log('Servidor Rodando...'))
+app.get('/usuarios', async (req, res) => {
+  const usuarios = await knex('usuarios').select('*');
+  res.json(usuarios);
+});
+app.post('/usuarios', async (req, res) => {
+ const { nome, email } = req.body;
+ await knex('usuarios').insert({ nome, email });
+ res.status(201).json({ mensagem: 'Usuário criado com sucesso' });
+});
+app.listen(3000, () => {
+ console.log('Servidor rodando na porta 3000');
+});
